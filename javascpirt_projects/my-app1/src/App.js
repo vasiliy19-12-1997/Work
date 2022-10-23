e7b73be28dd4835d931ed8cd1574d6e3d8ea9eb0
Vasiliy <Wasyok.ru@yandex.ru>
2022-10-23 15:15:28 +0500

    custom hook

--- javascpirt_projects/my-app1/src/App.js
+++ javascpirt_projects/my-app1/src/App.js
@@ -3,35 +3,17 @@ import PostItem from "./components/postItem/PostItem";
 import PostList from "./components/postList/PostList";
 import MyButton from "./components/ui/button/MyButton";
 import MyInput from "./components/ui/input/MyInput";
+import { usePosts } from "./components/hooks/UsePosts";
 
 import PostForm from "./components/postForm/PostForm";
 import MySelect from "./components/ui/select/MySelect";
 import PostFilter from "./components/postFilter/PostFilter";
 import ClassCounter from "./components/counter/ClassCounter";
+import MyModal from "./components/ui/myModal/MyModal";
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Js", body: "Description" },
-    { id: 2, title: "Ts", body: "Description" },
-    { id: 3, title: "Python", body: "Description" },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-
-  const sortedPosts = useMemo(() => {
-    console.log("play functipn");
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLocaleLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
-
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -42,6 +24,7 @@ function App() {
 
   return (
     <div>
+      {/* <MyModal></MyModal> */}
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
