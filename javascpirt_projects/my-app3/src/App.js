37fa6ba8550e860830ca3d4c6f27b2d7aa5ac533
Vasili <Wasyok.ru@yandex.ru>
2022-11-14 21:18:48 +0500

    app3 map

--- javascpirt_projects/my-app3/src/App.js
+++ javascpirt_projects/my-app3/src/App.js
@@ -2,31 +2,18 @@ import React, { useState } from "react";
 import MyButton from "./components/ui/myButton/MyButton";
 import s from "./App.module.scss";
 import TodoForm from "./components/todoForm/TodoForm";
+import ToDoList from "./components/todoList/TodoList";
 function App() {
   const [todos, setTodos] = useState([
-    // { id: Math.random(), title: "Js", body: "language of programming1" },
-    // { id: Math.random(), title: "Java", body: "language of programming2" },
-    // { id: Math.random(), title: "Python", body: "language of programming3" },
+    { id: Math.random(), title: "Js", body: "language of programming1" },
+    { id: Math.random(), title: "Java", body: "language of programming2" },
+    { id: Math.random(), title: "Python", body: "language of programming3" },
   ]);
-  const createTodos = (newTodos) => {
-    setTodos([...todos, newTodos]);
-  };
+  const createTodos = (newTodos) => {};
   console.log(todos);
   return (
     <div className={s.App}>
-      {/* {todos.map((post, index) => (
-        <div>
-          <div>
-            <strong></strong>
-            <div></div>
-          </div>
-          <div>
-            <MyButton>Delete</MyButton>
-          </div>
-        </div>
-      ))} */}
-      <TodoForm create={createTodos} />
-
+      <ToDoList todos={todos}></ToDoList>
       <MyButton>Удалить</MyButton>
     </div>
   );
