f4155054a9a7f82999be07e990634c1ed2367e60
Vasili <Wasyok.ru@yandex.ru>
2022-11-22 12:55:15 +0500

    search and sort

--- javascpirt_projects/my-app3/src/App.js
+++ javascpirt_projects/my-app3/src/App.js
@@ -14,8 +14,9 @@ function App() {
     { id: 2, body: "Description", title: "Typescript" },
     { id: 3, body: "Description", title: "Python" },
   ]);
-  const [selectedSort, setSelectedSort] = useState("");
-  // const [filter, setFilter] = useState({ sort: "", query: "" });
+  // const [selectedSort, setSelectedSort] = useState("");
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  // const [searchQuery, setSearchQuery] = useState("");
 
   // const [edit, setEdit] = useState(false);
   // const [post, setPosts] = useState([]);
@@ -25,10 +26,20 @@ function App() {
   const deleteTodos = (todo) => {
     setTodos(todos.filter((p) => p.id !== todo.id));
   };
-  const sortedTodos = (sort) => {
-    setSelectedSort(sort);
-    setTodos([...todos].sort((a, b) => a[sort].localeCompare(b[sort])));
-  };
+  const sortedTodos = useMemo(() => {
+    if (filter.sort) {
+      return [...todos].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return todos;
+  }, [filter.sort, todos]);
+  const sortedAndSearchTodos = useMemo(() => {
+    return sortedTodos.filter((todo) =>
+      todo.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  });
+  console.log(sortedAndSearchTodos);
 
   // const editTodos = (todo) => {
   //   setTodos(todos.filter((t) => t.id === todo.id));
@@ -39,7 +50,7 @@ function App() {
     <div className={s.App}>
       <TodoForm create={createTodos} />
       <MySelect
-        value={selectedSort}
+        value={filter.sort}
         onChange={sortedTodos}
         defaultValue={"Sort by"}
         options={[
@@ -48,8 +59,12 @@ function App() {
         ]}
       />
       {/* <TodoClassForm create={createTodos} /> */}
-      <ToDoList remove={deleteTodos} todos={todos} title="Todo"></ToDoList>
-      {/* <TodoFilter filter={filter} setFilter={setFilter} /> */}
+      <ToDoList
+        remove={deleteTodos}
+        todos={sortedAndSearchTodos}
+        title="Todo"
+      ></ToDoList>
+      <TodoFilter filter={filter} setFilter={setFilter} />
     </div>
   );
 }
