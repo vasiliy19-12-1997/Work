13b1ac3f319eef29370b7d37aa504644fd3276db
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2022-12-22 18:30:48 +0500

    Delete Education directory

--- Education/react learn/foandament_react2/src/router/index.jsx
+++
@@ -1,9 +0,0 @@
-import About from "../pages/About"
-import Posts from "../pages/Posts"
-import PostIdPage from "../pages/PostIdPage"
-export const routes = [
-    {path: '/about' , component: About},
-    {path: '/posts', component: Posts},
-    {path: '/posts/:id', component: PostIdPage},
-]
-
--- Education/typescript/todo_list_2/src/data/stores/helper.tsx
+++
@@ -1,4 +0,0 @@
-type GenerateId = () => string;
-export const generateId: GenerateId = () => (
-    Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
-);
