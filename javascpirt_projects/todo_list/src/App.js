b08b35758e4bc0ab22688aba4e7aef7774b2d4df
Vasiliy <Wasyok.ru@yandex.ru>
2023-01-01 12:22:59 +0500

    navigate

--- javascpirt_projects/todo_list/src/App.js
+++ javascpirt_projects/todo_list/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, Navigate } from "react-router-dom";
 import About from "./components/pages/About";
 import Todos from "./components/pages/Todos";
 import Navbar from "./components/ui/navbar/Navbar";
+import Error from "./components/pages/Error";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route element={<About />} path="/about"></Route>
         <Route element={<Todos />} path="/"></Route>
+        <Route element={<Error />} path="/error"></Route>
+        {/* если пользователь введет не существующий url, то перейдем к туду листу */}
+        <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
